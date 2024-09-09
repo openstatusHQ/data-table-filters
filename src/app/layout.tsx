4d15a6d654cf06ae7e38a854a506a2fd55a231d7
Maximilian Kaske <maximilian@kaske.org>
2024-09-09 20:59:44 +0200

    chore: next-themes and quick links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -42,7 +43,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import { columns } from "@/_data-table/columns";
 import { data, filterFields } from "@/_data-table/constants";
 import { DataTable } from "@/_data-table/data-table";
 import { searchParamsCache } from "@/_data-table/search-params";
+import { ModeToggle } from "@/components/theme/toggle-mode";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Github, Globe, Twitter } from "lucide-react";
 
 export default function Page({
   searchParams,
@@ -12,8 +15,31 @@ export default function Page({
   const search = searchParamsCache.parse(searchParams);
 
   return (
-    <main className="container mx-auto flex min-h-screen flex-col gap-4 px-2 py-4 md:px-4 md:py-8">
-      <div className="relative min-h-full h-full rounded-lg border border-border/50 bg-background/50 p-4 backdrop-blur-[2px] sm:p-8">
+    <main className="container mx-auto flex min-h-screen flex-col gap-4 p-4 sm:p-16">
+      <div className="sm:sticky top-0 flex w-full max-w-7xl mx-auto">
+        <div className="sm:absolute sm:top-2 sm:-right-12 gap-2 p-1 flex-1 flex sm:flex-col justify-center items-center">
+          <ModeToggle />
+          <Button variant="ghost" size="sm" className="w-9 px-0" asChild>
+            <a
+              href="https://github.com/openstatusHQ/data-table-filters"
+              target="_blank"
+            >
+              <Github />
+            </a>
+          </Button>
+          <Button variant="ghost" size="sm" className="w-9 px-0" asChild>
+            <a href="https://twitter.com/openstatusHQ" target="_blank">
+              <Twitter />
+            </a>
+          </Button>
+          <Button variant="ghost" size="sm" className="w-9 px-0" asChild>
+            <a href="https://openstatus.dev" target="_blank">
+              <Globe />
+            </a>
+          </Button>
+        </div>
+      </div>
+      <div className="w-full max-w-7xl mx-auto relative min-h-full h-full rounded-lg border border-border/50 bg-background/50 p-4 backdrop-blur-[2px] sm:p-8">
         <DataTable
           columns={columns}
           data={data}
