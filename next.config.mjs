0dec47ab611d0641a8c4e7a6cbfae759ebdd37d3
Maximilian Kaske <maximilian@kaske.org>
2024-09-29 09:46:32 +0200

    chore: add redirect to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+				source: "/i",
+				destination: "/infinite",
+				permanent: true
+			}
+		]
+	}
+};
 
 export default nextConfig;
