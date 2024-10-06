8bd39768ee32fa34436c54b60ded89761873097f
Maximilian Kaske <maximilian@kaske.org>
2024-10-06 17:12:37 +0200

    fix: type

--- src/react-table.d.ts
+++ src/react-table.d.ts
@@ -6,8 +6,14 @@ declare module "@tanstack/react-table" {
     cellClassName?: string;
     label?: string;
   }
+
   interface FilterFns {
-    inDateRange: FilterFn<any>;
-    arrSome: FilterFn<any>;
+    inDateRange?: FilterFn<any>;
+    arrSome?: FilterFn<any>;
+  }
+
+  // https://github.com/TanStack/table/discussions/4554
+  interface ColumnFiltersOptions<TData extends RowData> {
+    filterFns?: Record<string, FilterFn<TData>>;
   }
 }
