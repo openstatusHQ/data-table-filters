8c00b7ecfb611b2aa460b443a21c69f719871c13
Maximilian Kaske <maximilian@kaske.org>
2025-01-26 17:27:56 +0100

    fix: column order

--- src/components/data-table/data-table-view-options.tsx
+++ src/components/data-table/data-table-view-options.tsx
@@ -17,7 +17,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 import {
   Sortable,
@@ -39,20 +39,6 @@ export function DataTableViewOptions<TData>({
   const [search, setSearch] = useState("");
 
   const columnOrder = table.getState().columnOrder;
-
-  const columns = useMemo(
-    () =>
-      table
-        .getAllColumns()
-        .filter(
-          (column) =>
-            typeof column.accessorFn !== "undefined" && column.getCanHide()
-        )
-        .sort((a, b) => {
-          return columnOrder.indexOf(a.id) - columnOrder.indexOf(b.id);
-        }),
-    [table, columnOrder]
-  );
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -80,7 +66,7 @@ export function DataTableViewOptions<TData>({
             {/* TODO: add a "RESET REORDERING ROW" */}
             <CommandGroup>
               <Sortable
-                value={columns.map((c) => ({ id: c.id }))}
+                value={table.getAllColumns().map((c) => ({ id: c.id }))}
                 onValueChange={(items) =>
                   table.setColumnOrder(items.map((c) => c.id))
                 }
@@ -89,39 +75,54 @@ export function DataTableViewOptions<TData>({
                 onDragEnd={() => setDrag(false)}
                 onDragCancel={() => setDrag(false)}
               >
-                {columns.map((column) => (
-                  <SortableItem key={column.id} value={column.id} asChild>
-                    <CommandItem
-                      value={column.id}
-                      onSelect={() =>
-                        column.toggleVisibility(!column.getIsVisible())
-                      }
-                      className={"capitalize"}
-                      disabled={drag}
-                    >
-                      <div
-                        className={cn(
-                          "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
-                          column.getIsVisible()
-                            ? "bg-primary text-primary-foreground"
-                            : "opacity-50 [&_svg]:invisible"
-                        )}
+                {table
+                  .getAllColumns()
+                  .filter(
+                    (column) =>
+                      typeof column.accessorFn !== "undefined" &&
+                      column.getCanHide()
+                  )
+                  .sort((a, b) => {
+                    return (
+                      columnOrder.indexOf(a.id) - columnOrder.indexOf(b.id)
+                    );
+                  })
+                  .map((column) => (
+                    <SortableItem key={column.id} value={column.id} asChild>
+                      <CommandItem
+                        value={column.id}
+                        onSelect={() =>
+                          column.toggleVisibility(!column.getIsVisible())
+                        }
+                        className={"capitalize"}
+                        disabled={drag}
                       >
-                        <Check className={cn("h-4 w-4")} />
-                      </div>
-                      <span>{column.columnDef.meta?.label || column.id}</span>
-                      {enableOrdering && !search ? (
-                        <SortableDragHandle
-                          variant="ghost"
-                          size="icon"
-                          className="size-5 ml-auto text-muted-foreground hover:text-foreground focus:bg-muted focus:text-foreground"
+                        <div
+                          className={cn(
+                            "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                            column.getIsVisible()
+                              ? "bg-primary text-primary-foreground"
+                              : "opacity-50 [&_svg]:invisible"
+                          )}
                         >
-                          <GripVertical className="size-4" aria-hidden="true" />
-                        </SortableDragHandle>
-                      ) : null}
-                    </CommandItem>
-                  </SortableItem>
-                ))}
+                          <Check className={cn("h-4 w-4")} />
+                        </div>
+                        <span>{column.columnDef.meta?.label || column.id}</span>
+                        {enableOrdering && !search ? (
+                          <SortableDragHandle
+                            variant="ghost"
+                            size="icon"
+                            className="size-5 ml-auto text-muted-foreground hover:text-foreground focus:bg-muted focus:text-foreground"
+                          >
+                            <GripVertical
+                              className="size-4"
+                              aria-hidden="true"
+                            />
+                          </SortableDragHandle>
+                        ) : null}
+                      </CommandItem>
+                    </SortableItem>
+                  ))}
               </Sortable>
             </CommandGroup>
           </CommandList>
