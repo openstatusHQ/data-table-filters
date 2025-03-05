772f4da699d282edcdff979e75835b39c26e04b7
Maximilian Kaske <maximilian@kaske.org>
2025-03-05 15:37:10 +0100

    chore: guide

--- src/app/guide/layout.tsx
+++ src/app/guide/layout.tsx
@@ -1,14 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function MDXLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="container mx-auto px-4 py-8">
+      <Button asChild variant="link" className="-ml-4">
+        <Link href="/">
+          <ChevronLeft className="mr-2 h-4 w-4" /> Back
+        </Link>
+      </Button>
+      <Separator className="my-6" />
       <div
         className={cn(
-          "prose dark:prose-invert prose-lg",
+          "prose prose-lg mx-auto dark:prose-invert",
           "prose-pre:bg-foreground dark:prose-pre:bg-muted/50",
-          "prose-figure:border prose-figure:border-border prose-figure:rounded-lg",
-          "prose-blockquote:pe-6 prose-blockquote:not-italic prose-blockquote:bg-muted/50 prose-blockquote:rounded-lg prose-blockquote:border prose-blockquote:border-border prose-blockquote:font-normal"
+          "prose-figure:rounded-lg prose-figure:border prose-figure:border-border",
+          "prose-blockquote:rounded-lg prose-blockquote:border prose-blockquote:border-border prose-blockquote:bg-muted/50 prose-blockquote:pe-6 prose-blockquote:font-normal prose-blockquote:not-italic",
         )}
       >
         {children}
