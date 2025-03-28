ce118aaf8fb446f73cbd0bce103ec2a5824820c0
Maximilian Kaske <maximilian@kaske.org>
2025-03-28 12:53:06 +0100

    feat: button pile

--- src/app/light/layout.tsx
+++ src/app/light/layout.tsx
@@ -15,18 +15,36 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </Button>
       {children}
       <div className="fixed bottom-4 right-4 z-50">
-        <Button asChild className="group">
-          <a
-            href="https://github.com/openstatusHQ/data-table-filters"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span className="mr-1">View GitHub Repo</span>
-            <ArrowRight className="relative mb-[1px] inline h-4 w-0 transition-all group-hover:w-4" />
-            <ChevronRight className="relative mb-[1px] inline h-4 w-4 transition-all group-hover:w-0" />
-          </a>
-        </Button>
+        <ButtonPile />
       </div>
     </>
+  );
+}
+
+function ButtonPile() {
+  return (
+    <div className="group/pile relative pt-1.5">
+      <Button asChild className="group">
+        <a
+          href="https://github.com/openstatusHQ/data-table-filters"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="mr-1">View GitHub Repo</span>
+          <ArrowRight className="relative mb-[1px] inline h-4 w-0 transition-all group-hover:w-4" />
+          <ChevronRight className="relative mb-[1px] inline h-4 w-4 transition-all group-hover:w-0" />
+        </a>
+      </Button>
+      <Button
+        asChild
+        className="group absolute -right-1.5 top-0 -z-10 opacity-70 transition-transform group-hover/pile:-translate-x-1.5 group-hover/pile:-translate-y-10 group-hover/pile:opacity-100"
+      >
+        <a href="https://light.openstatus.dev" target="_blank" rel="noreferrer">
+          <span className="mr-1">Explore Light OS</span>
+          <ArrowRight className="relative mb-[1px] inline h-4 w-0 transition-all group-hover:w-4" />
+          <ChevronRight className="relative mb-[1px] inline h-4 w-4 transition-all group-hover:w-0" />
+        </a>
+      </Button>
+    </div>
   );
 }
