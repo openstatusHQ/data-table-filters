8ac960e73bf489d06f00f4fbd812da0c3137afa7
Maximilian Kaske <maximilian@kaske.org>
2026-03-08 17:23:46 +0100

    fix: build

--- src/app/drizzle/page.tsx
+++ src/app/drizzle/page.tsx
@@ -2,6 +2,9 @@ import { getQueryClient } from "@/providers/get-query-client";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Client } from "./client";
 
+// Force dynamic rendering to avoid caching issues
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <HydrationBoundary state={dehydrate(getQueryClient())}>
