7827dc5ad2aacae8cb365a82d79c9a2d4e4babdb
Maximilian Kaske <maximilian@kaske.org>
2026-03-08 17:11:31 +0100

    fix: drizzle db migration

--- src/db/drizzle/drizzle.config.ts
+++ src/db/drizzle/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/db/drizzle/schema.ts",
-  out: "./drizzle",
+  out: "./src/db/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
