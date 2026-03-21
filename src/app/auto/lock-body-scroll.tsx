fb6c51d0439e90b3e8fc52defc523f797dae2866
Maximilian Kaske <maximilian@kaske.org>
2026-03-21 12:23:06 +0100

    fix: remove useeffect

--- src/app/auto/lock-body-scroll.tsx
+++ src/app/auto/lock-body-scroll.tsx
@@ -1,13 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-
 export function LockBodyScroll() {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, []);
-  return null;
+  return <style>{`body { overflow: hidden; }`}</style>;
 }
